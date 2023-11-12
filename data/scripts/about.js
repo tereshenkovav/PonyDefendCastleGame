44f59c74fd27daf4fc5ff43bc48e5260d0bd1802
TAV <tereshenkov@mail.ru>
2023-11-12 12:05:32 +0300

    Список благодарностей убран из репозитория, теперь он берется из общего реестра. При отсутствии файла credits.json, программа работает корректно.

--- data/scripts/about.js
+++ data/scripts/about.js
@@ -36,8 +36,9 @@ function Init() {
 
    credits_str = "" ;
    var credits = system.loadObject("credits.json") ;
-   for (var i=0; i<credits.length; i++)
-     credits_str+=(credits[i]+"\n") ;
+   if (credits!=null)
+     for (var i=0; i<credits.length; i++)
+       credits_str+=(credits[i]+"\n") ;
 
    return true ;
 }
